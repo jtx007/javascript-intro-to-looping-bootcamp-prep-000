d5752371489d28556cb98ea3812e3c43e1eae910
James Thomas <jamesjacobthomas7@gmail.com>
2018-03-15 03:19:13 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,13 @@
+
+function forLoop(array) {
+  for (i = 0, i < 25, i++) {
+    if (i == 0) {
+      array.push("I am 1 strange loop.")
+      return array
+    } else {
+      array.push(`I am ${i} strange loop.`)
+      return aray
+    }
+  }
+
+}
